acfb5542d0b44bd84ee5845541ff7a7ee61a7a24
kwangure <kaf.wangure@gmail.com>
2021-11-22 16:14:44 +0300

    feat: cleanup layout

--- src/apps/dashboard/routes/__layout.svelte
+++ src/apps/dashboard/routes/__layout.svelte
@@ -17,12 +17,23 @@
 
 <style>
     .layout {
+        --page-width: 92%;
         height: 100%;
+        font-family: 'Inter', var(--br-font-family);
         display: grid;
         grid-template-rows: max-content 1fr;
     }
-    .layout :global(.berry-navbar) {
-        --br-nav-border: var(--br-border);
+    .layout :global(.berry-navbar.berry-navbar) {
+        --br-nav-max-width: 57rem;
+        --br-nav-width: var(--page-width);
+        --br-nav-height: 72px;
+        --br-nav-block-padding: 0px;
+    }
+    main {
+        max-width: 56rem;
+        width: var(--page-width);
+        margin-left: auto;
+        margin-right: auto;
     }
     img {
         display: block;
